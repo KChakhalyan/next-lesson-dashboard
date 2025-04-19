9d63f7cff0319f6783b67c34b23eb66f18b5cbeb
KChakhalyan <karenchakhalyan23@gmail.com>
2025-04-19 17:58:53 +0400

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function CustomersPage() {
+    return (
+        <div className="flex h-full w-full flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold">Customers Page</h1>
+            <p className="text-gray-500">This is the customers page.</p>
+            <p className="text-gray-500">You can add your customers here.</p>
+        </div>);
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="flex h-full w-full flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <p className="text-gray-500">This is the dashboard page.</p>
+        </div>);
+}
